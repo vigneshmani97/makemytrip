2386c28f2255e9ed26919196b29cbc9318f9984e
Tamilarasan <Tamilarasan@192.168.1.42>
2023-03-11 20:34:55 +0530

    first commit

--- selenium/src/selenium/Pageobjectmodel.java
+++ selenium/src/selenium/Pageobjectmodel.java
@@ -1,24 +1,42 @@
 package selenium;
 
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Pageobjectmodel {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\chromedriver110.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.instagram.com/");
 		driver.manage().window().maximize();
+		
+		
+		String oldwnd=driver.getWindowHandle();
+		driver.getWindowHandle();
 
 		//loginpage loginpage= new loginpage();
 		loginpage.username(driver).sendKeys("vignesh");
 		loginpage.password(driver).sendKeys("vignesh");
 		loginpage.loginbtn(driver).click();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		loginpage.frgtpswd(driver).click();
+		Set<String> handles=driver.getWindowHandles();
+		for (String newwdw : handles) {
+			//driver.switchTo().window(newwdw);
+		loginpage.creatnwac(driver).click();
+		loginpage.usrnm(driver).sendKeys("8838108829");
+		loginpage.fulnam(driver).sendKeys("vigneshMani");
+		loginpage.uname(driver).sendKeys("vigneshvicky9865543");
+		loginpage.pswd(driver).sendKeys("balusai03");
+		loginpage.sinup(driver).click();
 
+		}
 		/*WebElement username=driver.findElement(By.name("username"));
 		username.sendKeys("vigneshm");
 
@@ -29,7 +47,6 @@ public class Pageobjectmodel {
 		loginbtn.click();
 
 		driver.quit();*/
-
 
 
 
--- selenium/src/selenium/loginpage.java
+++ selenium/src/selenium/loginpage.java
@@ -9,18 +9,36 @@ public class loginpage {
 
 
 	public static WebElement  username(WebDriver driver) {
-		return driver.findElement(By.name("username"));
-
+		return driver.findElement(By.xpath("//input[@name='username']"));
 	}
 	public  static WebElement  password(WebDriver driver) {
-		return driver.findElement(By.name("password"));
-
+		return driver.findElement(By.xpath("//input[@name='password']"));
 	}
 	public static WebElement loginbtn(WebDriver driver) {
 		return driver.findElement(By.id("loginForm"));
-
 	}
-
-
+	
+	public static WebElement frgtpswd(WebDriver driver) {
+		return driver.findElement(By.xpath("//a[normalize-space()='Forgot password?']"));
+	}
+	public static WebElement creatnwac(WebDriver driver) {
+		return driver.findElement(By.xpath("//a[normalize-space()='Create new account']"));
+	}
+	public static WebElement usrnm(WebDriver driver) {
+		return driver.findElement(By.name("emailOrPhone"));
+	}
+	public static WebElement fulnam(WebDriver driver) {
+		return driver.findElement(By.name("fullName"));
+	}
+	public static WebElement uname(WebDriver driver){
+		return driver.findElement(By.name("username"));
+	}
+	public static WebElement pswd(WebDriver driver) {
+		return driver.findElement(By.name("password"));
+	}
+	public static WebElement sinup(WebDriver driver) {
+		return driver.findElement(By.xpath("//div[1]//div[1]//div[2]//form[1]//div[7]//div[1]"));
+		
+	}
 }
 
